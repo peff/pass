9fc49a14f7b5df98dc89441247444874be0b9106
Jeff King <peff@peff.net>
2014-04-17 16:10:58 -0400

    autofill: use DOM.XPath
    Early versions of the pentadactyl plugin used
    util.evaluatXPath, which is deprecated. I tried to address
    that when making the last commit, but ended up with
    DOM.evaluateXPath, which does not even exist! Use the
    correct name.

--- pentadactyl/autofill.js
+++ pentadactyl/autofill.js
@@ -10,7 +10,7 @@ function fill()
   for (i in fills) {
     let doc = window.content.document;
     if (!fills[i][0].test(doc.URL)) continue;
-    for (let elem in DOM.evaluateXPath('//input', doc)) {
+    for (let elem in DOM.XPath('//input', doc)) {
       if (elem.id.match(/user/i)) {
         elem.value = getField(fills[i][1] + ".user");
       }
