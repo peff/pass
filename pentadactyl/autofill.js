9636e3cab3921f9ab243a8aa404da04a4097b73d
Jeff King <peff@peff.net>
2014-05-22 12:13:18 -0400

    autofill: recognize "login" as a login box
    This seems obvious in retrospect, but most sites really do
    seem to use some variant of user/username.

--- pentadactyl/autofill.js
+++ pentadactyl/autofill.js
@@ -17,7 +17,7 @@ function fill()
     let doc = window.content.document;
     if (!fills[i][0].test(doc.URL)) continue;
     for (let elem in DOM.XPath('//input', doc)) {
-      if (elem.id.match(/user/i)) {
+      if (elem.id.match(/user|login/i)) {
         fillField(elem, fills[i][1] + ".user");
       }
       else if (elem.type == "password" && elem.id.match(/pass/i)) {
